a9e2951bbb1c70ed53ca01bbc56a9e54c0397453
Modestti Tomas <tomas.modestti@gmail.com>
2025-11-10 00:55:11 -0300

    cambio de ruta

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { useCartContext } from "../../context/CartContext/useCartContext";
+import { useCartContext } from "../../context/CartContext/UseCartContext";
 import "./Nav.css";
 
 
